deb0684488c23a4d4e73135e3459bf5f8e4eda5e
vishal <vishaltyagi.dev098@gmail.com>
2022-08-05 17:44:59 +0530

    finished login frontend

--- client/src/components/Account/Login.jsx
+++ client/src/components/Account/Login.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import {Box,TextField,Button,styled} from '@mui/material';
 import loginLogo from './login-logo.png';
 
@@ -9,16 +10,55 @@ const LoginContainer=styled(Box)`
   flex-direction:column;
 `;
 
+
+
 function Login() {
+  // login or signup state
+  const [login, setLogin] = useState(0);
+
+  // toggleLogin function
+  function toggleLogin(){
+    if(login===0){
+      setLogin(1);
+    }
+    else{
+      setLogin(0);
+    }
+  }
+
+
   return (
     <Box sx={{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center"}}>
-      <LoginContainer>
-        <img src={loginLogo} alt="Login Logo" width={"100%"}/>
-        <TextField id="standard-basic" label="Enter your name" variant="standard" sx={{margin:"10px 10px"}}/>
-        <TextField id="standard-basic" label="Enter your Password" variant="standard" sx={{margin:"10px 10px"}}/>
-        <Button variant="contained" sx={{margin:"10px 10px"}}>Login</Button>
-        <Button variant="text" sx={{margin:"10px 10px"}}>Create new account</Button>
-      </LoginContainer>
+      {
+        login===0 ?
+      
+          <LoginContainer>
+
+            <img src={loginLogo} alt="Login Logo" width={"100%"}/>
+
+            <TextField id="standard-basic" label="Enter your name" variant="standard" sx={{margin:"10px 10px"}}/>
+            <TextField id="standard-basic" label="Enter your Password" variant="standard" sx={{margin:"10px 10px"}}/>
+
+            <Button variant="contained" sx={{margin:"10px 10px"}}>Login</Button>
+            <Button variant="text" onClick={toggleLogin} sx={{margin:"10px 10px"}}>Create new account</Button>
+
+          </LoginContainer>
+    
+        :
+          <LoginContainer>
+
+            <img src={loginLogo} alt="Login Logo" width={"100%"}/>
+
+            <TextField id="standard-basic" label="Enter your name" variant="standard" sx={{margin:"10px 10px"}}/>
+            <TextField id="standard-basic" label="Enter username" variant="standard" sx={{margin:"10px 10px"}}/>
+            <TextField id="standard-basic" label="Enter your Password" variant="standard" sx={{margin:"10px 10px"}}/>
+
+            <Button variant="contained" sx={{margin:"10px 10px"}}>Signup</Button>
+            <Button variant="text" onClick={toggleLogin} sx={{margin:"10px 10px"}}>Already have an account?</Button>
+
+          </LoginContainer>
+
+    }
     </Box>
   )
 }
