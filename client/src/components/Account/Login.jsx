bfcf6be6467ee3f8ae89471c89508bc87beada2f
vishal <vishaltyagi.dev098@gmail.com>
2022-08-10 09:17:35 +0530

    updated login

--- client/src/components/Account/Login.jsx
+++ client/src/components/Account/Login.jsx
@@ -13,8 +13,17 @@ const LoginContainer=styled(Box)`
 
 
 function Login() {
+  // dummy initial value of signup state
+  const initalSignupValues={
+    name:"",
+    username:"",
+    password:"",
+  }
+
   // login or signup state
   const [login, setLogin] = useState(0);
+  // singup value state
+  const [signupValue, setsignupValue] = useState(initalSignupValues);
 
   // toggleLogin function
   function toggleLogin(){
@@ -24,6 +33,16 @@ function Login() {
     else{
       setLogin(0);
     }
+  }
+
+  // getSignupValue function
+  function getSignupValue(e){
+    setsignupValue({...signupValue,[e.target.name]:e.target.value});
+    // last alphabet is not taken by this function, fix it!
+    
+    // console.log(signupValue.name);
+    // console.log(signupValue.username);
+    // console.log(signupValue.password);
   }
 
 
@@ -49,9 +68,9 @@ function Login() {
 
             <img src={loginLogo} alt="Login Logo" width={"100%"}/>
 
-            <TextField id="standard-basic" label="Enter your name" variant="standard" sx={{margin:"10px 10px"}}/>
-            <TextField id="standard-basic" label="Enter username" variant="standard" sx={{margin:"10px 10px"}}/>
-            <TextField id="standard-basic" label="Enter your Password" variant="standard" sx={{margin:"10px 10px"}}/>
+            <TextField onChange={(e)=>getSignupValue(e)} name="name" id="standard-basic" label="Enter your name" variant="standard" sx={{margin:"10px 10px"}}/>
+            <TextField onChange={(e)=>getSignupValue(e)} name="username" id="standard-basic" label="Enter username" variant="standard" sx={{margin:"10px 10px"}}/>
+            <TextField onChange={(e)=>getSignupValue(e)} name="password" id="standard-basic" label="Enter your Password" variant="standard" sx={{margin:"10px 10px"}}/>
 
             <Button variant="contained" sx={{margin:"10px 10px"}}>Signup</Button>
             <Button variant="text" onClick={toggleLogin} sx={{margin:"10px 10px"}}>Already have an account?</Button>
