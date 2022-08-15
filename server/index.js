cf6b008763eaa6b038a122f6d75d26a79eb60396
vishal <vishaltyagi.dev098@gmail.com>
2022-08-15 22:26:15 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -4,4 +4,4 @@ const app=express();
 
 const PORT=8000;
 
-app.listen(PORT,()=>{console.log(`Server is running on port = ${PORT}`)});
+app.listen(PORT,()=>{console.log(`Server is running on port : ${PORT}`)});
